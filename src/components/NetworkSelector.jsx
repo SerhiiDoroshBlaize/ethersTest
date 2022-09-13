17147778567c571ade6a6752f4b46d5a77b086c0
SerhiiDoroshBlaize <serhii.d@blaize.tech>
2022-09-13 18:01:26 +0300

    added network list to NetworkSelector

--- src/components/NetworkSelector.jsx
+++ src/components/NetworkSelector.jsx
@@ -1,17 +1,38 @@
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import { useProvider } from "../context/Web3ProviderContext";
+import networkData from "../contract/networkData.json";
 
 const NetworkSelector = () => {
   const { provider } = useProvider();
   const [selectNetwork, setSelectNetwork] = useState();
   const getNetwork = async () => {
-    const chainId = await provider.send("eth_chainId");
+    const { chainId } = await provider.getNetwork()
     console.log(chainId);
-    setSelectNetwork(chainId);
+    setSelectNetwork( chainId );
   };
-  const switchNetwork = async (chainId) => {
-    await provider.send("wallet_switchEthereumChain", [{ chainId }]);
+  const switchNetwork = async (network) => {
+    const data = networkData.filter((el)=>{
+      return el.chainId === Number(network)
+    })[0]
+   
+    if(network === 1 || network ===3|| network === 4 || network ===5 || network === 42 ) {
+      await provider.send("wallet_switchEthereumChain", [
+        { chainId: `0x${Number(data.chainId).toString(16)}` },
+      ]);
+    } else {
+      await provider.send("wallet_addEthereumChain", [
+        {
+          chainId: `0x${Number(data.chainId).toString(16)}`,
+          rpcUrls: [data.rpc[0]],
+          chainName: data.name,
+          nativeCurrency: data.nativeCurrency,
+          blockExplorerUrls: data.explorers
+            ? [data.explorers[0].url]
+            : null,
+        },
+      ]);
+    }
   };
 
   useEffect(() => {
@@ -24,17 +45,22 @@ const NetworkSelector = () => {
     }
   }, [selectNetwork]);
   return (
-    <div>
-      <div>{`Current Network: ${selectNetwork}`}</div>
+    <div className='networkChainId'>
+      <div>{`Current Network: ${1}`}</div>
       <select
+      className='select'
         onChange={(e) => {
-          setSelectNetwork(e.target.value);
+          setSelectNetwork(Number(e.target.value));
         }}
         value={selectNetwork}
       >
-        <option value="0x4">0x4</option>
-        <option value="0x2a">0x2a</option>
-        <option value="0x3">0x3</option>
+        {networkData.map((el, i) => {
+          return (
+            <option key={i} value={el.chainId}>
+              {el.name}
+            </option>
+          );
+        })}
       </select>
     </div>
   );
